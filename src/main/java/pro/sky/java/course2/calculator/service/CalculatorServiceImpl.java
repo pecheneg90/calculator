32e1f5f530a062245110f481701350fccb7d835c
Vadim <manjavha@gmail.com>
2022-06-06 23:45:29 +0300

    calculator done!

--- src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
@@ -27,7 +27,7 @@ public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public String divide(int num1, int num2) {
         if (num2 == 0) {
-            throw new IllegalArgumentException("на ноль делить нельзя");
+            return "на ноль делить нельзя";
         }
         return String.format("%d / %d = %d", num1, num2, num1 / num2);
     }
